fc9a00572ae559e20bf78d8a21c9e5c6d7428615
mamunbban <mamun.itbban@gmail.com>
2024-09-23 17:15:19 +0600

    added a navbar section

---
+++ js/donation.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-donate-noakhali')
+         .addEventListener('click', function(event){
+            event.preventDefault()
+
+const donateNoakhali = document.getElementById('input-donate-noakhali').value;
+
+
+
+ 
+})
